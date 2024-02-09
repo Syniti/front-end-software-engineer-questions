258060f9ae92eee644bdf6012c3f38843d07f736
Aankirz <sahuankit453@gmail.com>
2024-02-09 14:14:51 +0530

    code responsiveness

--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
 
 const Navbar: React.FC = () => {
+  const [isSearchVisible, setIsSearchVisible] = useState(false);
+
+  const toggleSearch = () => {
+    setIsSearchVisible((prev) => !prev);
+  };
+
   return (
-    <nav className="bg-gray-800 py-4 px-6 flex items-center justify-between rounded absolute top-[2px] w-[80%]">
+    <nav className="bg-gray-800 py-4 px-6 flex items-center justify-between rounded-lg absolute top-[2px] w-full md:w-[80%]">
       <div className="flex items-center">
         <h1 className="text-white font-bold text-lg">SAAS</h1>
       </div>
       <div className="flex items-center">
-        <input
-          type="text"
-          placeholder="Search..."
-          className="bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none"
-        />
-        <button className="ml-2 bg-gray-700 text-white px-3 py-1 rounded-md hover:bg-gray-600 focus:outline-none">
-          Search
+        <button
+          className="bg-gray-700 text-white px-3 py-1 rounded-md hover:bg-gray-600 focus:outline-none h-[2rem]"
+          onClick={toggleSearch}
+        >
+          <FaSearch />
         </button>
+        {isSearchVisible && (
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none ml-2"
+          />
+        )}
       </div>
     </nav>
   );
--- client/app/components/services/UserStorage.tsx
+++ client/app/components/services/UserStorage.tsx
@@ -9,7 +9,7 @@ const UserStorage: React.FC<UserStorageProps> = ({ usedStorage, totalStorage }) 
   const percentage = Math.round((usedStorage / totalStorage) * 100);
 
   return (
-    <div className='flex flex-col items-center justify-center h-[14rem] w-[14rem] border-2 border-slate-500 rounded-lg p-4 bg-slate-700'>
+    <div className='flex flex-col items-center justify-center h-auto md:h-[14rem] w-[12rem] md:w-[14rem] border-2 border-slate-500 rounded-lg p-4 bg-slate-700'>
       <div className='text-white text-2xl font-bold mb-4'>Storage</div>
       <div className='text-4xl font-bold text-white mb-4'>{usedStorage} GB</div>
       <div className='relative w-full h-2 bg-white rounded-full mb-4'>
